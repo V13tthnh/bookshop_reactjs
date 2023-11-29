ace6abfe24e649a7a28ef284f093355a9fa74984
TranNghia281203 <146937143+TranNghia281203@users.noreply.github.com>
2023-11-29 14:46:10 +0700

    fix/css-js-login-register--thanhnghia

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,29 @@
-import "./App.css";
+//import "./App.css";
 import 'jquery/src/jquery';
 import React from "react";
-import { BrowserRouter,Routes, Route } from 'react-router-dom';
-import "./assets_2/css/bootstrap.min.css";
-import "./assets_2/css/normalize.css";
-import "./assets_2/css/font-awesome.min.css";
-import "./assets_2/css/icomoon.css";
-import "./assets_2/css/jquery-ui.css";
-import "./assets_2/css/owl.carousel.css";
-import "./assets_2/css/transitions.css";
+import {Routes, Route } from 'react-router-dom';
+// import "./assets_2/css/bootstrap.min.css";
+// import "./assets_2/css/normalize.css";
+// import "./assets_2/css/font-awesome.min.css";
+// import "./assets_2/css/icomoon.css";
+// import "./assets_2/css/jquery-ui.css";
+// import "./assets_2/css/owl.carousel.css";
+// import "./assets_2/css/transitions.css";
 import "./assets_2/css/main.css";
-import "./assets_2/css/color.css";
-import "./assets_2/css/responsive.css";
+//import "./assets_2/css/color.css";
+//import "./assets_2/css/responsive.css";
 
-import './assets_2/js/vendor/jquery-library.js';
-import "./assets_2/js/vendor/bootstrap.min.js";
+// import './assets_2/js/vendor/jquery-library.js';
+// import "./assets_2/js/vendor/bootstrap.min.js";
 import './assets_2/js/owl.carousel.min.js';
-import "./assets_2/js/jquery.vide.min.js"
-import './assets_2/js/countdown.js';
-import './assets_2/js/jquery-ui.js';
-import './assets_2/js/parallax.js';
-import './assets_2/js/countTo.js';
-import './assets_2/js/appear.js';
-import './assets_2/js/gmap3.js';
-import './assets_2/js/main.js';
+// import "./assets_2/js/jquery.vide.min.js"
+// import './assets_2/js/countdown.js';
+// import './assets_2/js/jquery-ui.js';
+// import './assets_2/js/parallax.js';
+// import './assets_2/js/countTo.js';
+// import './assets_2/js/appear.js';
+//import './assets_2/js/gmap3.js';
+// import './assets_2/js/main.js';
 
 import HomePage from './pages/HomePage.js';
 import Page404 from "./pages/Page404.js";
